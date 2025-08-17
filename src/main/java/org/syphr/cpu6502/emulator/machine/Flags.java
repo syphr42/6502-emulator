a6bbdf0bcea9106a6b90b0f4286bdb7e0ad7bf1f
Gregory Moyer <greg@commitpush.run>
2025-08-17 19:26:23 -0400

    Refactor processor flags to match docs

--- src/main/java/org/syphr/cpu6502/emulator/machine/Flags.java
+++ src/main/java/org/syphr/cpu6502/emulator/machine/Flags.java
@@ -5,8 +5,8 @@ import lombok.Builder;
 @Builder(toBuilder = true)
 public record Flags(boolean negative,
                     boolean overflow,
-                    boolean user1,
-                    boolean user2,
+                    boolean user,
+                    boolean breakCommand,
                     boolean decimal,
                     boolean irqDisable,
                     boolean zero,
