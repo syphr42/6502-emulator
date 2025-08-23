744eb8621637350969da05e8d87a83f5165d9078
Gregory Moyer <greg@commitpush.run>
2025-08-22 23:12:26 -0400

    Remove ghz option from ClockSpeed

--- src/main/java/org/syphr/cpu6502/emulator/machine/ClockSpeed.java
+++ src/main/java/org/syphr/cpu6502/emulator/machine/ClockSpeed.java
@@ -7,7 +7,6 @@ public record ClockSpeed(Duration cycle)
     public static ClockSpeed ONE_HZ = new ClockSpeed(Duration.ofSeconds(1));
     public static ClockSpeed ONE_KHZ = new ClockSpeed(Duration.ofMillis(1));
     public static ClockSpeed ONE_MHZ = new ClockSpeed(Duration.ofNanos(1000));
-    public static ClockSpeed ONE_GHZ = new ClockSpeed(Duration.ofNanos(1));
 
     public ClockSpeed times(int multiplier)
     {
