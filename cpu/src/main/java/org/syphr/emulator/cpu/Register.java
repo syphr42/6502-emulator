8d210897c56e2c16f0a4bb32a929538fd9729a47
Gregory Moyer <greg@commitpush.run>
2025-09-13 16:14:20 -0400

    Implement AtomicReference for Register to allow multithread access

--- cpu/src/main/java/org/syphr/emulator/cpu/Register.java
+++ cpu/src/main/java/org/syphr/emulator/cpu/Register.java
@@ -15,47 +15,62 @@
  */
 package org.syphr.emulator.cpu;
 
-import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+
 @Slf4j
 @RequiredArgsConstructor
 @ToString(onlyExplicitlyIncluded = true)
-@EqualsAndHashCode
 class Register
 {
     @ToString.Include
-    private Value value = Value.ZERO;
+    private final AtomicReference<Value> value = new AtomicReference<>(Value.ZERO);
 
     public void decrement()
     {
-        value = value.decrement();
+        value.getAndUpdate(Value::decrement);
     }
 
     public void increment()
     {
-        value = value.increment();
+        value.getAndUpdate(Value::increment);
     }
 
     public boolean isNegative()
     {
-        return value.isNegative();
+        return value.get().isNegative();
     }
 
     public boolean isZero()
     {
-        return value.isZero();
+        return value.get().isZero();
     }
 
     public void store(Value value)
     {
-        this.value = value;
+        this.value.set(value);
     }
 
     public Value value()
     {
-        return value;
+        return value.get();
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == null || getClass() != o.getClass()) { return false; }
+        Register register = (Register) o;
+        return Objects.equals(value.get(), register.value.get());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return value.get().hashCode();
     }
 }
